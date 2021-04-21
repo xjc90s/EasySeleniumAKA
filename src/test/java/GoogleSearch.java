aff667b3d28653dc85c61b2df40bf371fddca299
xjc90s <xavier.cuadrado@gmail.com>
2021-04-21 16:13:12 +0200

    add chrome options to chrome driver

--- src/test/java/GoogleSearch.java
+++ src/test/java/GoogleSearch.java
@@ -3,12 +3,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GoogleSearch{
     @Test
     public void testGoogleSearch() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com/");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("enable-automation");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--disable-gpu");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
         Thread.sleep(5000);  // Let the user actually see something!
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("ChromeDriver");
